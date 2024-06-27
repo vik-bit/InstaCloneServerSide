2730e76c4e406ba37305dd9707016fb82aa9bfaf
vik-bit <vikrammohanty.royalstar12@gmail.com>
2024-06-27 20:49:56 +0530

    all done

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURI:"mongodb+srv://vikrammohantyroyalstar12:Starlord123@cluster0.owwqqrj.mongodb.net/",
+    JWT_SECRET:"eeeffekekekekgkkjhjsls",
+    SENDGRID_API:process.env.API_KEY,
+    EMAIL:process.env.EMAIL
+}
