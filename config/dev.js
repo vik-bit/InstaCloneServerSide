9d3eea29544f800444b0ae07e8d217c6ee9627ee
vik-bit <73734452+vik-bit@users.noreply.github.com>
2024-06-27 22:09:09 +0530

    Delete config/dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://vikrammohantyroyalstar12:Starlord123@cluster0.owwqqrj.mongodb.net/",
-    JWT_SECRET:"eeeffekekekekgkkjhjsls",
-    SENDGRID_API:process.env.API_KEY,
-    EMAIL:process.env.EMAIL
-}
